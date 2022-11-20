e7e95e8085bebd1f15b179d7711d34b426b6c0b2
yuma-matsui <oshino.iiyo@gmail.com>
2022-11-20 21:34:33 +0900

    簡易的なテストの追加

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,8 @@ import App from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const p = screen.getByRole('heading', { name: 'Hello' })
-  expect(p).toBeInTheDocument()
+  const h1 = screen.getByRole('heading', { name: 'Hello' })
+  const h2 = screen.getByRole('heading', { name: 'Good Bye' })
+  expect(h1).toBeInTheDocument()
+  expect(h2).toBeInTheDocument()
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { FC } from 'react'
 
-const App: FC = () => <h1>Hello</h1>
+const App: FC = () => (
+  <>
+    <h1>Hello</h1>
+    <h2>Good Bye</h2>
+  </>
+)
 
 export default App
