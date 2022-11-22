10c30141cb03aef5ea140d4da0fe8de90f6c8522
yuma-matsui <oshino.iiyo@gmail.com>
2022-11-22 20:22:55 +0900

    circleciの設定変更

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,8 @@ test('renders learn react link', () => {
   render(<App />)
   const h1 = screen.getByRole('heading', { name: 'Hello' })
   const h2 = screen.getByRole('heading', { name: 'Good Bye' })
+  const h3 = screen.getByRole('heading', { name: 'Good Night' })
   expect(h1).toBeInTheDocument()
   expect(h2).toBeInTheDocument()
+  expect(h3).toBeInTheDocument()
 })
--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ const App: FC = () => (
   <>
     <h1>Hello</h1>
     <h2>Good Bye</h2>
+    <h3>Good Night</h3>
   </>
 )
 
