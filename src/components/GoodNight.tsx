3428ffe3b59edc3756f97e3cda4d3bf4578f31c9
yuma-matsui <oshino.iiyo@gmail.com>
2022-11-26 10:50:57 +0900

    circleciが正常に動くか確認するためにテストケースを増やした

--- src/components/GoodNight.tsx
+++ src/components/GoodNight.tsx
@@ -1,5 +1,10 @@
 import { FC } from 'react'
 
-const GoodNight: FC = () => <h1>Good Night</h1>
+const GoodNight: FC = () => (
+  <>
+    <h1>Good Night</h1>
+    <h2>Good Evening</h2>
+  </>
+)
 
 export default GoodNight
--- src/components/__test__/GoodNight.test.tsx
+++ src/components/__test__/GoodNight.test.tsx
@@ -4,5 +4,8 @@ import GoodNight from '../GoodNight'
 test('GoodNightコンポーネント', () => {
   render(<GoodNight />)
   const goodNight = screen.getByRole('heading', { name: 'Good Night' })
+  const goodEvening = screen.getByRole('heading', { name: 'Good Evening' })
+
   expect(goodNight).toBeInTheDocument()
+  expect(goodEvening).toBeInTheDocument()
 })
