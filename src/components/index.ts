0808b360e1da741d0f2be2712914b9b7a4359d18
yuma-matsui <oshino.iiyo@gmail.com>
2022-11-26 12:26:35 +0900

    CDの実行検証のためにコンポーネントを追加した

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as GoodNight } from './GoodNight'
+export { default as GoodMorning } from './GoodMorning'
 export { default as Header } from './Header'
 export { default as Hello } from './Hello'
 export { default as TaskItem } from './TaskItem'
--- src/router/RouterConfig.tsx
+++ src/router/RouterConfig.tsx
@@ -1,13 +1,13 @@
 import { FC } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Welcome } from '../components'
-import GoodNight from '../components/GoodNight'
+import { GoodMorning, GoodNight, Welcome } from '../components'
 
 const RouterConfig: FC = () => (
   <BrowserRouter>
     <Routes>
       <Route index element={<Welcome />} />
       <Route path="/good_night" element={<GoodNight />} />
+      <Route path="/good_morning" element={<GoodMorning />} />
     </Routes>
   </BrowserRouter>
 )
