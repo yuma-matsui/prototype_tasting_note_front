14c03984d413b5ff6b5e4dcf54201d9a53f5d963
yuma-matsui <oshino.iiyo@gmail.com>
2022-12-01 20:13:02 +0900

    API用のURLの切り替え方法を変えた

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,14 +1,15 @@
 import axios from 'axios'
 import applyCaseMiddleware from 'axios-case-converter'
 
+const origin = process.env.REACT_APP_API_URL ?? 'https://api.tasting-note-staging.com'
+
 const options = {
   ignoreHeaders: true,
 }
 
 const client = applyCaseMiddleware(
   axios.create({
-    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-    baseURL: `${process.env.REACT_APP_API_URL}/api/v1`,
+    baseURL: `${origin}/api/v1`,
   }),
   options
 )
